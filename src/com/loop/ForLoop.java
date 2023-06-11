3b4b660489986e71f931c098dda16c0309fefe3a
Uma516 <hashmitha143@gmail.com>
2023-06-11 20:37:14 +0530

    secnd commit

--- src/com/loop/ForLoop.java
+++ src/com/loop/ForLoop.java
@@ -5,7 +5,7 @@ public class ForLoop {
     public static void main(String[] args){
 
     for (int a=1;a<=10;a++){
-        if(a%2==0){
+        if(a%2!=0){
             sum=sum+a;
         }
         }
