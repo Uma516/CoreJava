4e7cea101db880b86e351958dcb115ea9ab14a20
Uma516 <hashmitha143@gmail.com>
2023-06-13 12:21:38 +0530

    ternary operator

---
+++ src/Ternary.java
@@ -0,0 +1,9 @@
+public class Ternary {
+
+    public static void main(String[] args) {
+     int i=10;
+     String res= i%2==0 ? "EvenNumbber": "OddNumber";
+
+        System.out.println(res);
+    }
+}
