f200e1ec8ceda028442945d815bda9f6e6ba4f29
Uma516 <hashmitha143@gmail.com>
2023-06-26 18:59:44 +0530

    super and this keyword

---
+++ src/com/util/Test.java
@@ -0,0 +1,25 @@
+package com.util;
+
+public class Test {
+    int a=10;
+    int b=20;
+    void m1(){
+        System.out.println("m1 method");
+    }
+}
+class Test1 extends Test{
+    int a=11;
+    int b=12;
+    void m2(){
+        System.out.println("m2 method");
+        System.out.println(super.a+super.b);
+        System.out.println(this.a+this.b);
+    }
+    public static void main(String[] args){
+        Test1 t=new Test1();
+        t.m1();
+        t.m2();
+
+    }
+}
+
