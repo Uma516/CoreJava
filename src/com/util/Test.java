e8d0254e02ad3342a7cc9738beddbf05de11e88b
Uma516 <hashmitha143@gmail.com>
2023-06-27 19:31:28 +0530

    overloading

--- src/com/util/Test.java
+++ src/com/util/Test.java
@@ -1,31 +1,27 @@
 package com.util;
 
 public class Test {
-    int a=10;
-    int b=20;
-    void m1(){
-        System.out.println("m1 method");
+
+    void m1(int a){
+        System.out.println(10);
     }
-}
+    void m2(String str){
+        System.out.println("john");
+    }
+  }
 class Test1 extends Test{
-    int a=11;
-    int b=12;
-    void m2(){
-        System.out.println("m2 method");
-        System.out.println(super.a+super.b);
-        System.out.println(this.a+this.b);
+    void m1(double k){
+        System.out.println(10000);
+    }
+    void m2(float f){
+        System.out.println(1.5f);
     }
     public static void main(String[] args){
-        Test t1=new Test();
-        Object o=new Test1();
         Test1 t=new Test1();
-        t.m1();
-        t.m2();
-     System.out.println(t instanceof Test);
-     System.out.println(t instanceof Object);
-     System.out.println(t1 instanceof Object);
-     System.out.println(o instanceof Test1);
-     System.out.println(o instanceof Test);
+        t.m1(10000 );
+        t.m1(10);
+        t.m2("john");
+        t.m2(1.5f);
+
     }
 }
-
