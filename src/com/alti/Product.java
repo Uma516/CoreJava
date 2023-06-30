f6a04bd22750b0d49a3ecd2cba93ee80c7544e1c
Uma516 <hashmitha143@gmail.com>
2023-06-30 18:16:42 +0530

    overriding

--- src/com/alti/Product.java
+++ src/com/alti/Product.java
@@ -1,20 +1,25 @@
 package com.alti;
 
 public class Product {
-  void m1(int a){
+  void m1(int a) {
       System.out.println("m1 method");
+  }
+      void m2(char c){
+      System.out.println("m2 method");
   }
 }
 class Test extends Product{
-    void m1(String b,float f){
-        System.out.println("child method");
+    void m1(int a){
+        System.out.println("m1 method");
+    }
+    void m2(char c){
+        System.out.println("m2 method");
     }
     public static void main(String[] args){
         Product p =new Product();
         Test t=new Test();
-        t.m1("jon",1.5f);
+        t.m2('v');
         t.m1(10);
-        
     }
 }
 
