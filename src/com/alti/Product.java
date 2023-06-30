bdf993d4af2639f7299beb9eb718da22b87d16cb
Uma516 <hashmitha143@gmail.com>
2023-06-30 18:24:06 +0530

    overriding constructor

--- src/com/alti/Product.java
+++ src/com/alti/Product.java
@@ -1,6 +1,9 @@
 package com.alti;
 
 public class Product {
+    Product(){
+        System.out.println("default constructor");
+    }
   void m1(int a) {
       System.out.println("m1 method");
   }
@@ -9,6 +12,10 @@ public class Product {
   }
 }
 class Test extends Product{
+    Test(String str){
+        System.out.println("one argument constructor");
+
+    }
     void m1(int a){
         System.out.println("m1 method");
     }
@@ -17,9 +24,11 @@ class Test extends Product{
     }
     public static void main(String[] args){
         Product p =new Product();
-        Test t=new Test();
+        Test t=new Test("LILLY");
         t.m2('v');
         t.m1(10);
+        
+
     }
 }
 
