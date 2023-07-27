6ba99694316c46915ae492f2dcc86d020a28e663
Uma516 <hashmitha143@gmail.com>
2023-07-27 19:32:20 +0530

    exceptios using pipe symbol

--- src/com/util/Prize.java
+++ src/com/util/Prize.java
@@ -1,23 +1,18 @@
 package com.util;
 
+import java.util.Scanner;
+
 public class Prize {
-  void m3(){
-      System.out.println(10/0);
-  }
-  void m2(){
-       m3();
-  }
-  void m1(){
-      try{
-          m2();
-      }catch(ArithmeticException e){
-          System.out.println("ArithematicException propagation.....");
-      }
 
-      }
-      public static void main(String[] args){
-      Prize p=new Prize();
-      p.m1();
-
-  }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("print a number");
+        int n = s.nextInt();
+        try{
+            System.out.println(10/0 );
+            System.out.println("ratan".charAt(4));
+        }catch(ArithmeticException|StringIndexOutOfBoundsException e){
+            System.out.println("rest of the code");
+        }
+    }
 }
