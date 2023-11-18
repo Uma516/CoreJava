e47df61b1128aa3b015536aca8359004728bd9de
Uma516 <hashmitha143@gmail.com>
2023-11-18 10:58:02 +0530

    sum of numbers

--- src/com/logicalprograms/ForLoopDemo.java
+++ src/com/logicalprograms/ForLoopDemo.java
@@ -1,12 +1,15 @@
 package com.logicalprograms;
 
 public class ForLoopDemo {
-//printing 1-10 negative numbers
+//printing sum of 1-10 numbers
+    static  int sum;
 public static void main(String[] args) {
 
-    for(int i=1;i>=-10;i--){
-        System.out.println(i);
+    for(int i=1;i<=10;i++){
+        sum=sum+i;
+
     }
+    System.out.println(sum);
      }
         }
 
