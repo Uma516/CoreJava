c0e9e6de6c8d25f22e318c33e9a7f25331770c31
Uma516 <hashmitha143@gmail.com>
2023-11-18 10:47:05 +0530

    loop demo

---
+++ src/com/logicalprograms/ForLoopDemo.java
@@ -0,0 +1,11 @@
+package com.logicalprograms;
+
+public class ForLoopDemo {
+//printing hello world 5 times
+    public static void main(String[] args) {
+        for(int a=1;a<=5;a++){
+            System.out.println("hello world--->"+a);
+        }
+    }
+}
+
