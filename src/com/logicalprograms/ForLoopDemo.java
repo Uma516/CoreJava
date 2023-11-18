220f2e108d95241378125b75c07c438e5a012764
Uma516 <hashmitha143@gmail.com>
2023-11-18 17:59:47 +0530

    negative numbers from back

--- src/com/logicalprograms/ForLoopDemo.java
+++ src/com/logicalprograms/ForLoopDemo.java
@@ -1,10 +1,10 @@
 package com.logicalprograms;
 
 public class ForLoopDemo {
-//printing odd numbers
+static int subtraction;
 public static void main(String[] args) {
-    for(int i=2;i<=10;i+=2){
-        System.out.println(i);
+    for(int a=-10;a<0;a++){
+        System.out.println(a);
 }
 
      }
