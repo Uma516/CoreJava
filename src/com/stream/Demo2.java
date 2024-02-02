57168fa3ca668898e8ba1083681993d38b3e356e
Uma516 <hashmitha143@gmail.com>
2024-02-02 10:41:13 +0530

    stream list program

--- src/com/stream/Demo2.java
+++ src/com/stream/Demo2.java
@@ -1,12 +1,15 @@
 package com.stream;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Demo2 {
     public static void main(String[] args) {
 
-
-        Stream<Integer>stream=Stream.iterate(2,n->n+1);
-        stream.limit(5).filter(n->n%2!=0).forEach(System.out::println);
+        List<Integer> list= Arrays.asList(1,2,3,4,5,6);
+        list.stream().filter(n->n%2!=0).forEach(System.out::println);
+      
     }
 }
